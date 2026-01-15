363838640155ffc0e070d2344f99763cf76f23d0
tikhonsu <tikhonsu@gmail.com>
2026-01-15 15:56:30 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // IMPORTANT: set base to '/<repo>/' for GitHub Pages.
-// You can also edit it later after repo is created.
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/crm-dashboard/',
 })
